4468936be3246c394deb2c7dcb612140942c55e1
酥鱼 <suyu0214al@163.com>
2024-04-27 01:56:54 +0000

    重构接受猜测数字功能
    Signed-off-by: 酥鱼 <suyu0214al@163.com>

--- src/NGuessNumber.java
+++ src/NGuessNumber.java
@@ -11,35 +11,11 @@ public class NGuessNumber {
         System.out.println("欢迎来到新猜数字游戏！");
         while(true){
 
-            while(answers.size() < 4) {
-                int answer = (int)(Math.random() * 10);
-                if(!answers.contains(answer)){
-                    answers.add(answer);
-                }
-            }
+           answers = genereateAnswer();，
             while(true){
                 System.out.println("请输入4个0-9之间不重复的整数:");
                     guesses.clear();
-                while(guesses.size() < 4) {
-                    int guess;
-                    try{
-                        guess = scanner.nextInt();
-                    }catch (InputMismatchException e){
-                        System.out.println("请输入数字！");
-                        scanner.next();
-                        continue;
-                    }
-                    if(guess < 0 || guess > 9){
-                        System.out.println("请输入0-9之间的整数！");
-                        continue;
-                    }
-                    if(!guesses.contains(guess)){
-                        guesses.add(guess);
-                    }else {
-                        System.out.println("数字已经存在！");
-                    }
-                }
-
+               guesses = receiveGuesses();
                 int a = 0;
                 for(int i = 0; i < 4; i++){
                     if(answers.get(i) == guesses.get(i)){
@@ -67,6 +43,36 @@ public class NGuessNumber {
             }
         }
 
-
+        public static ArrayList<Integer> genereateAnswer() {
+ while(answers.size() < 4) {
+ int answer = (int)(Math.random() * 10);
+ if(!answers.contains(answer)){
+ answers.add(answer);
+ }
+ }
+ return answers;
+}
+public static ArrayList<Integer> receiveGuesses() {
+ while(guesses.size() < 4) {
+ int guess;
+ try{
+ guess = scanner.nextInt();
+ }catch (InputMismatchException e){
+ System.out.println("请输⼊数字！");
+ scanner.next();
+ continue;
+ }
+ if(guess < 0 || guess > 9){
+ System.out.println("请输⼊0-9之间的整数！");
+ continue;
+ }
+ if(!guesses.contains(guess)){
+ guesses.add(guess);
+ }else {
+ System.out.println("数字已经存在！");
+ }
+ }
+ return guesses;
+}
     }
 }
